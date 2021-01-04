5043b0d8e9defe41f2e29b042dd3f77d68235ee9
benbolthouse <benbolthouse@gmail.com>
2021-01-04 13:04:52 -0700

    Add dependency module express-validator; implement express route validation on userRouter (in progress); integrate csrf protection into GET routes rendering with csrf token for pug templates.

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,31 @@
 const express = require('express');
 const csrf = require('csurf');
-
+const { validationResult } = require('express-validator');
+const { userSignupValidator, userLoginValidator } = require('./__formValidators');
 const { User } = require('../db/models');
 
 const router = express.Router();
 
 const csrfProtection = csrf({ cookie: true });
 
-// GET: http://localhost:8080/users/signup
-// https://github.com/BenBolthouse/nice-bytes/wiki/Frontend-Routes#get-httpnicebytescomsignup
 //
-router.get('/signup', csrfProtection, (req, res, next) => {
-  
+// GET: http://localhost:8080/users/signup
+//
+router.get('/signup', userSignupValidator, csrfProtection, (req, res, next) => {
   const { username, emailAddress, password, confirmPassword } = req.params;
 
   // 1. Check to see if the user already exists in the user store
-  
-  // res.send('respond with a resource');
+  // const existingUser = User.findAll({where: {emailAddress: emailAddress}}) !== null;
+
+  res.render('sign-up', { csrf: req.csrfToken() });
+});
+
+//
+// GET: http://localhost:8080/users/login
+//
+router.get('/login', userLoginValidator, csrfProtection, (req, res, next) => {
+  //
+  res.render('log-in', { csrf: req.csrfToken() });
 });
 
 module.exports = router;
