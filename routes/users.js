3875bdc96352d1858a2f1c8141fd6d9579c7f3b2
benbolthouse <benbolthouse@gmail.com>
2021-01-04 11:54:34 -0700

    Add dependency csurf to project; begin work on user signup route.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const csrf = require('csurf');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const { User } = require('../db/models');
+
+const router = express.Router();
+
+const csrfProtection = csrf({ cookie: true });
+
+// GET: http://localhost:8080/users/signup
+// https://github.com/BenBolthouse/nice-bytes/wiki/Frontend-Routes#get-httpnicebytescomsignup
+//
+router.get('/signup', csrfProtection, (req, res, next) => {
+  
+  const { username, emailAddress, password, confirmPassword } = req.params;
+
+  // 1. Check to see if the user already exists in the user store
+  
+  // res.send('respond with a resource');
 });
 
 module.exports = router;
