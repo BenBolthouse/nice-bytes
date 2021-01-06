3cf8fc597d953adbcfe5b77f62db904e62f21b34
Austin Howard <austin.smithfield@gmail.com>
2021-01-06 01:50:14 +0000

    merged changes

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,8 @@ router.get('/', async function(req, res, next) {
   // Code below to implement when we have reviews seeded
   //const spots = await Spot.findAll({include: Review, limit: 10, order: [ "Review.stars", 'DESC' ]});
   const spots = await Spot.findAll({ limit: 10, order: [ 'name' ]});
-<<<<<<< HEAD
+
   res.render('index', { title: 'NiceBytes', spots, user: req.session.user, });
-=======
-  res.render('index', { title: 'NiceBytes', spots, user: req.session.user });
->>>>>>> 9b69c9bd15ec46407280aadac21e4b08c467fab0
 });
 
 module.exports = router;
