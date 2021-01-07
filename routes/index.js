c30c43dc1f1934dbfe07b8f3734c60f20b6d2a5c
benbolthouse <benbolthouse@gmail.com>
2021-01-07 11:33:05 -0700

    Create list items for home page spots list.

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,12 @@ const router = express.Router();
  * GET http://localhost:8080/
  */
 router.get('/', async function (req, res, next) {
-  const spots = await Spot.findAll({ include: { model: Review }, limit: 10, order: ['name'] });
-  res.render('index', { title: 'NiceBytes', user: req.session.user, spots });
+  const spots = await Spot.findAll({
+    include: { model: Review },
+    limit: 10,
+    order: ['name'],
+  });
+  res.render('index', { user: req.session.user, spots });
 });
 
 module.exports = router;
