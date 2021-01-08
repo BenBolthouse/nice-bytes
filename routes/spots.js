c386a55355c3a32eabab0f34e27e584cbbe6eab8
benbolthouse <benbolthouse@gmail.com>
2021-01-08 12:09:18 -0700

    Add pseudocode for guidance on spots.js

--- routes/spots.js
+++ routes/spots.js
@@ -13,17 +13,30 @@ router.get("/:id", async (req, res, next) => {
   const userId = req.session.auth.userId;
 
   const spot = await Spot.findByPk(id);
-  const reviews = await Review.findAll({ where: { spotId: id } });
+  // const reviews = await Review.findAll({ where: { spotId: id } });
+
+  // Optimize the above query to do eager load of Review
+
+
   const customCollections = await Collection.findAll({
     where: { userId: userId },
     attributes: { exclude: ["Want To Visit", "Have Visited"] },
   });
+
+
   const defaultCollections = await Collection.findAll({
     where: {
       userId: userId,
       name: { [Op.or]: ["Want To Visit", "Have Visited"] },
     },
   });
+
+  // 1. Query the database to get collections WHERE userId = req.session.user.id
+      // Save to array customCollections
+  // 2. In resulting array, find collections with name "Have Visited" and "Want To Visit"
+  // 3. Splice() the two collections above to a new array defaultCollections
+  // 4. End result is customCollections does not have elements from defaultCollections
+
   const { firstName, lastName, username, email } = req.session.user;
 
   res.render("spot", {
