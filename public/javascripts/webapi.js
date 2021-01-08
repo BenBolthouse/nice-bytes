8612d3c9b6214a4e3dcf3d031d170a07f9a437f7
benbolthouse <benbolthouse@gmail.com>
2021-01-08 13:19:26 -0700

    Rename api-collections.js to webapi.js; include webapi.js module in layout.pug; remove link to deleted script in spot.pug;

--- public/javascripts/webapi.js
+++ public/javascripts/webapi.js
@@ -1,33 +1,33 @@
 /**
- * Collection management utilities for API queries.
+ * Utilities to perform RESTful requests to the web api.
  */
 
 /**
  * Asynchronously adds a spot to a collection. User must be authenticated.
  * Invokes optional callbacks on success or error.
- * @param {Integer id value of the collection to add to} collectionId 
- * @param {Integer id value of the spot to add to a collection} spotId 
+ * @param {Integer id value of the collection to add to} collectionId
+ * @param {Integer id value of the spot to add to a collection} spotId
  * @param {DOM event target} target
  * @param {Callback on 200 range responses} success
  * @param {Callback on 400 and 500-range responses} error
  */
-const addToCollection = async (collectionId, spotId, target, success, error) => {
+const post = async (collectionId, spotId, target, success, error) => {
   try {
-    await fetch('http://localhost:8080/api/collections/spot', {
+    await fetch('/api/collections/spot', {
       method: 'post',
       headers: {
         'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         collectionId,
         spotId,
       }),
     });
-    return success();
+    return success(target);
   } catch (e) {
-    return error(e);
+    return error(target, e);
   }
 };
 
-export { addToCollection }
+export { post };
