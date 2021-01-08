c4c90df40d3f8956395d0b1af391a02f07b5deb0
benbolthouse <benbolthouse@gmail.com>
2021-01-08 12:02:21 -0700

    Require User in module to perform nested eager loading; tested and working.

--- routes/spots.js
+++ routes/spots.js
@@ -1,38 +1,23 @@
 const express = require('express');
-const { Spot, Review } = require('../db/models');
+const { Spot, Review, User } = require('../db/models');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.redirect('/');
+  res.redirect('/');
 });
 
 router.get('/:id', async (req, res, next) => {
-    const id = req.params.id;
-    const spot = await Spot.findByPk(id, {include: Review }) //working code, but missing User model 
-    
-    // IN PROGRESS NESTED EAGER LOADING TO INCLUDE USERS THROUGH REVIEWS 
-    // { include: Review },
-    // {    
-    //     include: [{ 
-    //         model: Users,
-    //         through: { 
-    //             model: Review
-    //         }
-    //     }]
-    // })
-    
-    // const spot = await Spot.findAll({ 
-    //     where: {id: id},
-    //     include: [{ 
-    //         model: Review,
-    //         include: [{ 
-    //             model: User,
-    //         }]
-    //     }]
-    // });
+  const id = req.params.id;
 
-    res.render('spot', { title: `${spot.name}`, spot});
-})
+  const spot = await Spot.findByPk(id, {
+    include: {
+      model: Review,
+      include: User,
+    },
+  });
+
+  res.render('spot', { spot });
+});
 
 module.exports = router;
