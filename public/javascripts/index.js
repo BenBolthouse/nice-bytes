08adb8f099cfaf6ba7b07d7ad38c4a5f27533344
benbolthouse <benbolthouse@gmail.com>
2021-01-08 14:08:23 -0700

    Convert index.js script in views/index.pug to a module; commit new script webapi.js; add include to webapi.js in index.js.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,12 @@
+/**
+ * Module loaded on views/index.pug.
+ */
+
+import { get } from './webapi.js';
+
+// handle loading overlay on dom load
 document.addEventListener('DOMContentLoaded', evt => {
-    const pageAwaiting = document.getElementById('pageAwaiting');
-    pageAwaiting.classList.remove('page__awaiting--show');
-    pageAwaiting.classList.add('page__awaiting--hide');
-})
+  const pageAwaiting = document.getElementById('pageAwaiting');
+  pageAwaiting.classList.remove('page__awaiting--show');
+  pageAwaiting.classList.add('page__awaiting--hide');
+});
