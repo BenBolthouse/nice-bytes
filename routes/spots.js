f25db6d8358f3fc4ab402cdff699dd64de4488e8
Ryan <rdmaloney3@gmail.com>
2021-01-08 14:02:20 -0500

    debugging user not defined bug with nested eager loading

--- routes/spots.js
+++ routes/spots.js
@@ -1,38 +1,42 @@
-const express = require('express');
-const { Spot, Review } = require('../db/models');
+const express = require("express");
+const { Spot, Review } = require("../db/models");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.redirect('/');
+router.get("/", (req, res) => {
+  res.redirect("/");
 });
 
-router.get('/:id', async (req, res, next) => {
-    const id = req.params.id;
-    const spot = await Spot.findByPk(id, {include: Review }) //working code, but missing User model 
-    
-    // IN PROGRESS NESTED EAGER LOADING TO INCLUDE USERS THROUGH REVIEWS 
-    // { include: Review },
-    // {    
-    //     include: [{ 
-    //         model: Users,
-    //         through: { 
-    //             model: Review
-    //         }
-    //     }]
-    // })
-    
-    // const spot = await Spot.findAll({ 
-    //     where: {id: id},
-    //     include: [{ 
-    //         model: Review,
-    //         include: [{ 
-    //             model: User,
-    //         }]
-    //     }]
-    // });
+router.get("/:id", async (req, res, next) => {
+  const id = req.params.id;
+  //const spot = await Spot.findByPk(id, {include: Review }) //working code, but missing User model
 
-    res.render('spot', { title: `${spot.name}`, spot});
-})
+  const spot = await Spot.findAll({
+    where: { id: id },
+    include: [
+      {
+        model: Review,
+        include: [
+          {
+            model: User,
+          },
+        ],
+      },
+    ],
+  });
+
+  // IN PROGRESS NESTED EAGER LOADING TO INCLUDE USERS THROUGH REVIEWS
+  // { include: Review },
+  // {
+  //     include: [{
+  //         model: Users,
+  //         through: {
+  //             model: Review
+  //         }
+  //     }]
+  // })
+
+  res.render("spot", { title: `${spot.name}`, spot });
+});
 
 module.exports = router;
