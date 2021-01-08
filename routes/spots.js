1483ff68430d427988c9910dd39fd2f0bac01b7c
benbolthouse <benbolthouse@gmail.com>
2021-01-08 14:35:11 -0700

    Get correct version of spots router off the main branch.

--- routes/spots.js
+++ routes/spots.js
@@ -1,45 +1,19 @@
-const express = require("express");
-const { Spot, Review, Collection } = require("../db/models"); // Changed
-const { Op } = require("sequelize"); //Changed
+const express = require('express');
+const { Spot, Review } = require('../db/models');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.redirect("/");
+router.get('/', (req, res) => {
+    res.redirect('/');
 });
 
-router.get("/:id", async (req, res, next) => {
-  const id = req.params.id;
-  const userId = req.session.auth.userId;
+router.get('/:id', async (req, res, next) => {
+    const id = req.params.id;
 
-  const spot = await Spot.findByPk(id);
-  const reviews = await Review.findAll({ where: { spotId: id } });
-  const customCollections = await Collection.findAll({
-    where: { userId: userId },
-    attributes: { exclude: ["Want To Visit", "Have Visited"] },
-  });
-  const defaultCollections = await Collection.findAll({
-    where: {
-      userId: userId,
-      name: { [Op.or]: ["Want To Visit", "Have Visited"] },
-    },
-  });
-  const { firstName, lastName, username, email } = req.session.user;
+    const spot = await Spot.findByPk(id);
+    const reviews = await Review.findAll({where: { spotId: id}});
 
-  res.render("spot", {
-    title: `${spot.name}`,
-    spot,
-    reviews,
-    user: {
-      firstName,
-      lastName,
-      username,
-      email,
-      customCollections: [customCollections],
-      defaultCollections: [defaultCollections],
-    },
-  });
-});
+    res.render('spot', { title: `${spot.name}`, spot, reviews, user: req.session.user });
+})
 
 module.exports = router;
-  
