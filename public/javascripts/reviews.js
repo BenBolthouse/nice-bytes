ebba917db0e3640e13dac251912ec67db1f07d22
Ryan <rdmaloney3@gmail.com>
2021-03-30 18:05:20 -0400

    cleaned up comments and console.logs

--- public/javascripts/reviews.js
+++ public/javascripts/reviews.js
@@ -6,50 +6,6 @@ import { post, get, put, destroy } from "./webapi.js";
 
 // sync form to javascript
 document.addEventListener("DOMContentLoaded", (evt) => {
-  // handles the DOM star selection experience
-  // let rating;
-
-  // const userReviewStarColor = document.getElementById('userReviewStarColor');
-
-  // [
-  //   document.getElementById('userReviewStar1Selection'),
-  //   document.getElementById('userReviewStar2Selection'),
-  //   document.getElementById('userReviewStar3Selection'),
-  //   document.getElementById('userReviewStar4Selection'),
-  //   document.getElementById('userReviewStar5Selection'),
-  // ].forEach((star, i) => {
-  //   const pos = i + 1;
-  //   star.addEventListener('mouseover', evt => {
-  //     userReviewStarColor.classList.add(`star${pos}Active`);
-  //   });
-  //   star.addEventListener('mouseout', evt => {
-  //     userReviewStarColor.classList.remove(`star${pos}Active`);
-  //   });
-  //   star.addEventListener('click', evt => {
-  //     if (!rating) {
-  //       userReviewStarColor.classList.remove(`star${pos}Active`);
-  //       userReviewStarColor.classList.add(`star${pos}ActivePermanent`);
-  //       // userReviewForm.classList.add('ratingSelected');
-  //       // userReviewForm.classList.remove('ratingNotSelected');
-  //       rating = pos;
-  //     }
-  //     else {
-  //       userReviewStarColor.classList.remove(`star${pos}ActivePermanent`);
-  //       userReviewStarColor.classList.add(`star${pos}Active`);
-  //       // userReviewForm.classList.add('ratingNotSelected');
-  //       // userReviewForm.classList.remove('ratingSelected');
-  //       rating = null;
-  //     }
-  //   });
-  // });
-
-  // TODO gather form data and handle submit
-  const userReviewForm = document.getElementById("userReviewForm");
-
-  // userReviewForm.addEventListener('submit', evt => {
-  //     evt.preventDefault();
-  // });
-
   const editReviewBtn = document.getElementById("editReviewBtn");
   const deleteReviewBtn = document.getElementById("deleteReviewBtn");
 
@@ -67,9 +23,6 @@ document.addEventListener("DOMContentLoaded", (evt) => {
   const success = (reviewId, data) => {
     const review = document.getElementById(`review__${reviewId}`);
     review.parentNode.removeChild(review);
-    // target.parentNode.parentNode.parentNode.removeChild(
-    //   target.parentNode.parentNode
-    // );
   };
   const error = (target, error) => {
     console.log("delete error");
