9ace0d1c3a5bc6444c231e7308b677569a86c0c0
Ben Bolthouse <benbolthouse@gmail.com>
2021-03-30 18:28:13 -0700

    Add docs and spots review averaging functionality

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
+
 const { Spot, Review } = require("../db/models");
+
 const router = express.Router();
 
-//query the DB to find Spots
-
-/**
- * GET http://localhost:8080/
+/**********************************************************
+ * Returns a Pug view with spots data; the home page of the app.
  */
-router.get("/", asyncHandler(async (req, res, next) => {
+// prettier-ignore
+router.get("/", asyncHandler(async (req, res, _next) => {
+    // get spots and reviews
     const spots = await Spot.findAll({
       include: { model: Review },
       order: ["name"],
     });
+    // get average star rating per spot
+    spots.forEach((spot) => {
+      spot.rating = 0;
+      spot.Reviews.forEach((review) => {
+        spot.rating += review.stars;
+      });
+      if (spot.rating) {
+        const avg = spot.rating / spot.Reviews.length;
+        spot.stars = Math.ceil(avg);
+      }
+    });
+    // send pug view with spots data
     req.session.reload(() => {
       res.render("index", { user: req.session.user, spots });
     });
