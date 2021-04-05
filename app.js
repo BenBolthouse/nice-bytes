e35f6f23b22cbe1fefab5da16e37c5158eee3b31
Ben Bolthouse <benbolthouse@gmail.com>
2021-04-05 14:19:09 -0700

    Run formatter and alphabetize imports

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-const { sequelize } = require('./db/models');
-const { secret } = require('./config');
-const createError = require('http-errors');
-const authRouter = require('./routes/auth');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users')
-const spotRouter = require('./routes/spots');
-const collectionRouter = require('./routes/collection');
-const apiRouter = require('./routes/api');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const session = require('express-session');
-const logger = require('morgan');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const apiRouter = require("./routes/api");
+const authRouter = require("./routes/auth");
+const collectionsRouter = require("./routes/collections");
+const cookieParser = require("cookie-parser");
+const createError = require("http-errors");
+const express = require("express");
+const indexRouter = require("./routes/index");
+const logger = require("morgan");
+const path = require("path");
+const secret = require("./config").secret;
+const sequelize = require("./db/models").sequelize;
+const session = require("express-session");
+const spotRouter = require("./routes/spots");
+const usersRouter = require("./routes/users");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 
 // view engine setup
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
 // application config
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // set up session middleware
 const store = new SequelizeStore({ db: sequelize });
 app.use(
   session({
-    name: 'nice-bytes.sid',
+    name: "nice-bytes.sid",
     secret,
     store,
     saveUninitialized: false,
@@ -42,12 +42,12 @@ app.use(
 store.sync();
 
 // application routes
-app.use('/', indexRouter);
-app.use('/', authRouter);
-app.use('/api', apiRouter);
-app.use('/spots', spotRouter);
-app.use('/users', usersRouter);
-app.use('/collections', collectionRouter);
+app.use("/", indexRouter);
+app.use("/", authRouter);
+app.use("/api", apiRouter);
+app.use("/spots", spotRouter);
+app.use("/users", usersRouter);
+app.use("/collections", collectionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -58,11 +58,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
